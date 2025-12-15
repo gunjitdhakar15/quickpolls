0c1822fd5996727f343c4ad19088803d2f42ca22
snowleaopardxt <gunjit15@gmail.com>
2025-12-15 22:59:17 +0530

    Database connected. Environmental Variables defined

---
+++ server/database/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DB_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    }
+    
+    catch(err){
+        console.error('MongoDB Connection Error: ', err.message);
+        process.exit();
+    }
+};
